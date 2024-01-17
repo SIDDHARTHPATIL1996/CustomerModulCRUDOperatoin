7df2c3446982b70b2e3c317fb422231934148471
Siddharth <1996siddharthspatil@gmail.com>
2024-01-17 13:40:44 +0530

    Sql Script Added

---
+++ CustomerModulCRUDOperatoinTest/SqlScript/SqlScript.sql
@@ -0,0 +1,13 @@
+﻿use SqlPractice
+go
+Create Table Customer
+(
+CustomerId int Primary Key Identity,
+FirstName varchar (20),
+LastName varchar (20),
+MobileNumber char(10),
+City varchar (20),
+)
+insert into Customer Values('Siddharth', 'Patil', '9561459665', 'Pune')
+go
+Select * from Customer
